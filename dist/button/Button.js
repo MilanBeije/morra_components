a126d4f18f5415c81c8f0c3a297f88f5896bc260
MilanBeije <milan.stojkovic@beije.it>
2022-11-22 09:40:45 +0100

    button created

---
+++ dist/button/Button.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _reactNative = require("react-native");
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var Button = function Button() {
+  return /*#__PURE__*/_react.default.createElement(_reactNative.View, null, /*#__PURE__*/_react.default.createElement(_reactNative.TouchableOpacity, null, /*#__PURE__*/_react.default.createElement(_reactNative.Text, null, " Button")));
+};
+var _default = Button;
+exports.default = _default;
