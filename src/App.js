7b50359826448ea9ad0d8c95a70f38c51f33c274
pater97 <giuseppepater997@gmail.com>
2022-11-22 12:07:00 +0100

    update gameresult

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Text } from "react-native";
 import CustomButton from "./components/CustomButton";
+import { GameResult } from "./components/GameResult";
 
 function App() {
+
   return (
     <div className="App">
       <CustomButton
         onClickCallback={() => {
-          console.log("Ciao");
+          const risultato = GameResult(1,2)
+          console.log(risultato[0]);
         }}
       />
     </div>
